0851ee63eded3b7c357407fd2af5bfe8d856e117
Isaiah <zaymgmt707@gmail.com>
2024-07-15 13:59:44 -0700

    updates index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// Code your solution in this file!
+// Constant variable to store the company name
 const companyName = 'Scuber'
+// Variable to store the most profitable neighborhood
 let mostProfitableNeighborhood = 'Chelsea'
+// Variable to store the name of the company's CEO
 let companyCeo = 'Susan Smith'
